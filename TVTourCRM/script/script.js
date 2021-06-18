b6312ea47efade9da298878d6f2a48dcd2c53293
Pierre Sylvain <37309859+TiPete@users.noreply.github.com>
2021-06-18 07:47:19 -0400

    Add files via upload

---
+++ TVTourCRM/script/script.js
@@ -0,0 +1,33 @@
+function BoutonTechnique() {
+    loadHtml("mainWindow", ".//includes//main-technique.html");
+} 
+function BoutonResidences() {
+    loadHtml("mainWindow", ".//includes//main-residences.html");
+} 
+function BoutonGroupes() {
+    loadHtml("mainWindow", ".//includes//main-groupes.html");
+} 
+function BoutonPersonnes() {
+    loadHtml("mainWindow", ".//includes//main-personnes.html");
+} 
+function OpenPopUp(texte) {
+    document.getElementById("mainPopUp").innerHTML=texte;
+    document.getElementById("popUp").style.display="block";
+}
+const loadHtml = function(parentElementId, filePath) {
+    const init = {
+        method : "GET",
+        headers : { "Content-Type" : "text/html" },
+        mode : "cors",
+        cache : "default"
+    };
+    
+    const req = new Request(filePath, init);
+        fetch(req)
+        .then(function(response) {
+            return response.text();
+        })
+        .then(function(body) {
+        document.getElementById(parentElementId).innerHTML = body;
+        });
+    };
