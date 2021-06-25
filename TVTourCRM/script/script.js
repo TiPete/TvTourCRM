53189209c889f8a1fe506cfb8dfb812506910a12
Pierre Sylvain <37309859+TiPete@users.noreply.github.com>
2021-06-25 14:01:13 -0400

    Add files via upload

--- TVTourCRM/script/script.js
+++ TVTourCRM/script/script.js
@@ -1,15 +1,29 @@
 function BoutonTechnique() {
     loadHtml("mainWindow", ".//includes//main-technique.html");
+    resetSubMenus();
+    document.getElementById("techniqueSubMenu").style.display="block";
 } 
 function BoutonResidences() {
     loadHtml("mainWindow", ".//includes//main-residences.html");
+    resetSubMenus();
+    document.getElementById("residencesSubMenu").style.display="block";
 } 
 function BoutonGroupes() {
     loadHtml("mainWindow", ".//includes//main-groupes.html");
+    resetSubMenus();
+    document.getElementById("groupesSubMenu").style.display="block";
 } 
 function BoutonPersonnes() {
     loadHtml("mainWindow", ".//includes//main-personnes.html");
+    resetSubMenus();
+    document.getElementById("personnesSubMenu").style.display="block";
 } 
+function resetSubMenus() {
+    document.getElementById("techniqueSubMenu").style.display="none";
+    document.getElementById("residencesSubMenu").style.display="none";
+    document.getElementById("groupesSubMenu").style.display="none";
+    document.getElementById("personnesSubMenu").style.display="none";
+}
 
 function OpenPopUp(lien) {
     console.log(lien);
@@ -17,7 +31,6 @@ function OpenPopUp(lien) {
     document.getElementById("mainWindow").style.brightness= "50%";
     document.getElementById("popUp-container").style.display="block";
 }
-
 function ClosePopUp() {
     document.getElementById("mainWindow").style.backgroundColor= "white";
     document.getElementById("popUp-container").style.display="none";
